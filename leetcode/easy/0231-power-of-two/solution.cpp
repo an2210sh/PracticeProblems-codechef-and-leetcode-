a9ea4a4eac36286e8e25d6e6ff6c52291fb96e54
Ansh Srivastava <anshsriv.2001@gmail.com>
2026-09-02 00:54:45 +0530

    ✅ Power of Two | Easy | C++ | Runtime: 0 ms (100.00%) | Memory: 7.9 MB | 2026-09-01

--- leetcode/easy/0231-power-of-two/solution.cpp
+++ leetcode/easy/0231-power-of-two/solution.cpp
@@ -3,9 +3,7 @@ public:
     bool isPowerOfTwo(int n) {
         if(n==1)
         return true;
-        if(n==0)
-        return false;
-        if(n%2!=0)
+        if(n%2!=0 ||n==0)
         return false;
         return isPowerOfTwo(n/2);
     }
